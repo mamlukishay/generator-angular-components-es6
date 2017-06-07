5ffc6673c5babd13edf66570ac7bc6dd7888d284
Ishay Mamluk <ishay@ourcrowd.com>
2017-06-07 08:41:59 +0300

    component work

--- component/templates/_component.js
+++ component/templates/_component.js
@@ -4,7 +4,7 @@ const template = require('./company-page-x.html');
 
 export const companyPageXComponent = {
   template,
-  controller: class <%= componentClassName %> {
+  controller: class <%= componentClassName %>Component {
     constructor() {
       'ngInject';
     }
